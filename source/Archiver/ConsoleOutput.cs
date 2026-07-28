0b7602ab42843c1f5638129603c085bc1946633c
Karl Krupp <karl.krupp2002@gmail.com>
2026-07-28 17:20:56 +0200

    add more error messages for the user

--- source/Archiver/ConsoleOutput.cs
+++ source/Archiver/ConsoleOutput.cs
@@ -6,36 +6,57 @@
         {
             Console.WriteLine(
                 $@"{parsedArgument} is not a valid primary argument.
-                please use a main command as the primary command, a list of main commands:");
-            WritePrimaryArgumentsList();
-        }
-
-        public static void WriteWrongSecondaryArgumentsError(string parsedArgument)
-        {
-            Console.WriteLine(
-                $@"{parsedArgument} is not a valid secundary argument.
-                please use a valid secondary command after the main command:");
-            WriteSecundaryArgumentsList();
-        }
-
-        private static void WritePrimaryArgumentsList()
-        {
-            Console.WriteLine(
-                $@"-c or --create (creates an archive)
+                please use a main command as the primary command, a list of main commands:
+                -c or --create (creates an archive)
                 -a or --append (appends things to an existing archive)
                 -x or --extract (extraxts everything from an existing archive)
                 -i or --info (shows information about the archive)
                 -l or --list (shows the file names of the archives content)");
         }
 
-        private static void WriteSecundaryArgumentsList()
+        public static void WriteWrongSecondaryArgumentsError(string parsedArgument)
         {
             Console.WriteLine(
-                $@"-rle or --rleCompress (compresses an archive with the rle compression)
+                $@"{parsedArgument} is not a valid secundary argument.
+                please use a valid secondary command after the main command:
+                -rle or --rleCompress (compresses an archive with the rle compression)
                 -r or --retry and a number between 1 and 10 (used to specify the numbers of retries after a failed attempted)
                 -w or --wait and a number between 1 and 10 (used to specify the time between retries in seconds)
                 -s or --source and a path
                 -d or --destination and a path");
+        }
+
+        public static void WriteMissingParameterForArgumentError(string parsedArgument)
+        {
+            Console.WriteLine(
+                $@"{parsedArgument} should follow a describing parameter:
+                -r or --retry and a number between 1 and 10 (used to specify the numbers of retries after a failed attempted)
+                -w or --wait and a number between 1 and 10 (used to specify the time between retries in seconds)
+                -s or --source and a path
+                -d or --destination and a path");
+        }
+
+        public static void WriteNoErrorOccured(string parsedArgument)
+        {
+            Console.WriteLine($"no error occured at {parsedArgument}");
+        }
+
+        public static void WriteNoArgumentsToParse(string parsedArgument)
+        {
+            Console.WriteLine(
+$@"please use one of the following commands as the primary command:
+    -c or --create (creates an archive)
+    -a or --append (appends things to an existing archive)
+    -x or --extract (extraxts everything from an existing archive)
+    -i or --info (shows information about the archive)
+    -l or --list (shows the file names of the archives content)
+
+after that you can use the secundary commands:
+    -rle or --rleCompress (compresses an archive with the rle compression)
+    -r or --retry and a number between 1 and 10 (used to specify the numbers of retries after a failed attempted)
+    -w or --wait and a number between 1 and 10 (used to specify the time between retries in seconds)
+    -s or --source and a path
+    -d or --destination and a path");
         }
     }
 }
