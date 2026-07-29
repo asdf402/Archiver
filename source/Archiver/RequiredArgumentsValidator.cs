9f0ee258819eba905a9cce0bd996d50fc3c6c6e8
Karl Krupp <karl.krupp2002@gmail.com>
2026-07-29 17:01:50 +0200

    add requred arguments validator logic

--- source/Archiver/RequiredArgumentsValidator.cs
+++ source/Archiver/RequiredArgumentsValidator.cs
@@ -1,15 +1,134 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Archiver
+﻿namespace Archiver
 {
     public class RequiredArgumentsValidator : IValidator
     {
+        private readonly CommandDefaults commandDefaults;
+
+        public RequiredArgumentsValidator()
+        {
+            this.commandDefaults = new CommandDefaults();
+        }
+
         public bool IsValid(ParsedArguments parsedArguments)
         {
-            throw new NotImplementedException();
+            switch (parsedArguments.MainCommand)
+            {
+                case MainCommands.Create:
+                    return this.CreateValidation(parsedArguments);
+
+                case MainCommands.Append:
+                    return this.AppendValidation(parsedArguments);
+
+                case MainCommands.Extract:
+                    return this.ExtractValidation(parsedArguments);
+
+                case MainCommands.Info:
+                    return this.InfoValidation(parsedArguments);
+
+                case MainCommands.List:
+                    return this.ListValidation(parsedArguments);
+
+                default:
+                    return false;
+            }
+        }
+
+        // allowes: -rle, -r, -w
+        private bool CreateValidation(ParsedArguments parsedArguments)
+        {
+            // needs: -s, -d
+            if (this.HasSource(parsedArguments.Source) &&
+                this.HasDestination(parsedArguments.Destination))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // allowes: -rle, -r, -w
+        private bool AppendValidation(ParsedArguments parsedArguments)
+        {
+            // needs: -s, -d
+            if (this.HasSource(parsedArguments.Source) &&
+                this.HasDestination(parsedArguments.Destination))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // allowes: -rle, -r, -w
+        private bool ExtractValidation(ParsedArguments parsedArguments)
+        {
+            // needs: -s, -d
+            if (this.HasSource(parsedArguments.Source) &&
+                this.HasDestination(parsedArguments.Destination))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // allowes: -r, -w
+        private bool InfoValidation(ParsedArguments parsedArguments)
+        {
+            // needs: -s
+            if (this.HasSource(parsedArguments.Source))
+            {
+                // dont needs: -d, rle
+                if (!this.HasDestination(parsedArguments.Destination) ||
+                    !this.HasRle(parsedArguments.RleCompress))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // allowes: -r, -w
+        private bool ListValidation(ParsedArguments parsedArguments)
+        {
+            // needs: -s
+            if (this.HasSource(parsedArguments.Source))
+            {
+                // dont needs: -d, rle
+                if (!this.HasDestination(parsedArguments.Destination) ||
+                    !this.HasRle(parsedArguments.RleCompress))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool HasSource(string source)
+        {
+            if (source == this.commandDefaults.Path)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool HasDestination(string destination)
+        {
+            if (destination == this.commandDefaults.Path)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool HasRle(bool rle)
+        {
+            return rle;
         }
     }
 }
