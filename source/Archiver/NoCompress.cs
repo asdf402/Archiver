cb9e9d97ed61a9716d3134bb7fc7c0110ca69d90
Karl Krupp <karl.krupp2002@gmail.com>
2026-08-13 14:49:59 +0200

    add a compress interface and a no compress strategy

---
+++ source/Archiver/NoCompress.cs
@@ -0,0 +1,29 @@
+﻿namespace Archiver
+{
+    public class NoCompress : ICompress
+    {
+        /// <summary>
+        /// Does not do any compression. Writes the data from reader to the writer FileStream.
+        /// </summary>
+        /// <param name="reader">The FileStream from where the data will be read.</param>
+        /// <param name="writer">The FileStream to where the data will be written.</param>
+        /// <returns>Returns the not compressed size of the file.</returns>
+        public long Execute(BinaryReader reader, BinaryWriter writer)
+        {
+            int readBytes = 0;
+            long readBytesSum = readBytes;
+            int byteArraySize = 81920;
+            byte[] fileData = new byte[byteArraySize];
+
+            do
+            {
+                readBytes = reader.Read(fileData, 0, byteArraySize);
+                writer.Write(fileData, 0, readBytes);
+
+                readBytesSum += readBytes;
+            } while (readBytes > 0);
+
+            return readBytesSum;
+        }
+    }
+}
