f1b0d0bf52b911b62e4333b58ec1d5f09c06b204
Karl Krupp <karl.krupp2002@gmail.com>
2026-08-13 14:47:40 +0200

    renamed the archive class and restructured it

---
+++ source/Archiver/ArchiveInformation.cs
@@ -0,0 +1,25 @@
+﻿namespace Archiver
+{
+    using System.Text;
+
+    public class ArchiveInformation
+    {
+        private readonly string archiveIdentifier = "ARCHIVE";
+
+        public byte[] ArchiveIdentifier
+        {
+            get
+            {
+                return Encoding.UTF8.GetBytes(this.archiveIdentifier);
+            }
+        }
+
+        public long ArchiveIdentifierLength
+        {
+            get
+            {
+                return Encoding.UTF8.GetByteCount(this.archiveIdentifier);
+            }
+        }
+    }
+}
