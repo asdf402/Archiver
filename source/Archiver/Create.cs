5030986c8e083749af218fd32ddc203ce649fd6d
Karl Krupp <karl.krupp2002@gmail.com>
2026-09-16 21:33:11 +0200

    add the creation of an archive logic for the archive create command after restructuring

--- source/Archiver/Create.cs
+++ source/Archiver/Create.cs
@@ -5,13 +5,27 @@
         public Result Execute(ParsedArguments parsedArguments)
         {
             Append append = new Append();
+            ArchiveWriter archiveWriter = new ArchiveWriter();
             MetaInformation metaInformation = new MetaInformation(
                 DateTime.UtcNow,
                 parsedArguments.Compress,
                 0,
                 0);
 
-            return append.AppendFiles(parsedArguments, metaInformation, FileMode.CreateNew);
+            try
+            {
+                FileStream destination = new FileStream(parsedArguments.Destination, FileMode.CreateNew);
+                using (BinaryWriter binaryWriter = new BinaryWriter(destination))
+                {
+                    archiveWriter.WriteMetaInformation(binaryWriter, metaInformation);
+                }
+            }
+            catch (IOException)
+            {
+                return new Result(true, ConsoleErrorOutput.WriteCouldNotOpenDestination, parsedArguments.Destination);
+            }
+
+            return append.AppendFiles(parsedArguments, metaInformation, new MetaInformationPositions().EndOfMetaInformation);
         }
     }
 }
