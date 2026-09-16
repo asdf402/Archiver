3b091dd0cfb09bac591fd01f8aa6ab5cd85c94e8
Karl Krupp <karl.krupp2002@gmail.com>
2026-09-16 21:33:34 +0200

    fixed the append logic. now passes all tests

--- source/Archiver/Append.cs
+++ source/Archiver/Append.cs
@@ -4,6 +4,8 @@
     {
         public Result Execute(ParsedArguments parsedArguments)
         {
+            long endOfFilePosition;
+
             ArchiveReader archiveReader = new ArchiveReader();
             MetaInformation metaInformation;
 
@@ -12,67 +14,97 @@
                 FileStream destination = new FileStream(parsedArguments.Destination, FileMode.Open);
                 using (BinaryReader binaryReader = new BinaryReader(destination))
                 {
-                    metaInformation = new MetaInformation(
-                        new DateTime(0, 0, 0, 0, 0, 0),
-                        archiveReader.ReadCompressType(binaryReader),
-                        archiveReader.ReadFileAmount(binaryReader),
-                        archiveReader.ReadFileSizeUncompressed(binaryReader));
+                    metaInformation = archiveReader.ReadAllMetaInformation(binaryReader);
+                    endOfFilePosition = binaryReader.BaseStream.Length;
                 }
             }
             catch (IOException)
             {
                 return new Result(true, ConsoleErrorOutput.WriteCouldNotOpenDestination, parsedArguments.Destination);
-                throw;
             }
 
-            return this.AppendFiles(parsedArguments, metaInformation, FileMode.Open);
+            return this.AppendFiles(parsedArguments, metaInformation, endOfFilePosition);
         }
 
-        public Result AppendFiles(ParsedArguments parsedArguments, MetaInformation metaInformation, FileMode destinationFileMode)
+        public Result AppendFiles(ParsedArguments parsedArguments, MetaInformation metaInformation, long startingPosition)
         {
+            long fileSizeCompressedPosition;
+
             FileInformation fileInformation;
             ArchiveWriter archiveWriter = new ArchiveWriter();
-
-            long fileSizeCompressedPosition;
+            EnumerationOptions enumerationOptions = new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                ReturnSpecialDirectories = false,
+                MaxRecursionDepth = 100,
+            };
 
             try
             {
-                IEnumerable<string> files = Directory.EnumerateFiles(parsedArguments.Source);
+                int counter = 0;
 
-                FileStream destination = new FileStream(parsedArguments.Destination, destinationFileMode);
+                // gets all directories including root directory
+                IEnumerable<string> directories =
+                    Directory.GetDirectories(parsedArguments.Source, "*", enumerationOptions)
+                    .Prepend(parsedArguments.Source);
+
+                FileStream destination = new FileStream(parsedArguments.Destination, FileMode.Open);
                 using (BinaryWriter binaryWriter = new BinaryWriter(destination))
                 {
-                    foreach (var file in files)
+                    // changes the stream position to the given starting position
+                    binaryWriter.BaseStream.Position = startingPosition;
+
+                    // gets the next directory from the directories enumerable
+                    string? nextDirectory = this.GetNextDirectory(directories, counter);
+
+                    // checks if there are still directories left
+                    while (nextDirectory != null)
                     {
-                        fileSizeCompressedPosition = binaryWriter.BaseStream.Position;
+                        // gets every file from the directory
+                        IEnumerable<string> files = Directory.EnumerateFiles(nextDirectory);
 
-                        fileInformation = new FileInformation(
-                            Path.GetRelativePath(parsedArguments.Source, file),
-                            new FileInfo(file).Length,
-                            0);
-
-                        archiveWriter.WriteFileInformation(binaryWriter, fileInformation);
-
-                        try
+                        foreach (var file in files)
                         {
-                            FileStream source = new FileStream(parsedArguments.Source, FileMode.Open);
-                            using (BinaryReader binaryReader = new BinaryReader(source))
+                            // saves the position to later write the file size compressed there
+                            fileSizeCompressedPosition = binaryWriter.BaseStream.Position;
+
+                            // creates the new file object with all the necessary info
+                            fileInformation = new FileInformation(
+                                Path.GetRelativePath(parsedArguments.Source, file),
+                                new FileInfo(file).Length,
+                                0);
+
+                            // writes the info to the file
+                            archiveWriter.WriteFileInformation(binaryWriter, fileInformation);
+
+                            try
                             {
-                                fileInformation.AddFileSizeCompressed(
-                                    metaInformation.Compress.Execute(binaryReader, binaryWriter));
+                                // writes the file itself in the archive
+                                FileStream source = new FileStream(file, FileMode.Open);
+                                using (BinaryReader binaryReader = new BinaryReader(source))
+                                {
+                                    fileInformation.AddFileSizeCompressed(
+                                        metaInformation.Compress.Execute(binaryReader, binaryWriter));
+                                }
                             }
-                        }
-                        catch (IOException)
-                        {
-                            return new Result(true, ConsoleErrorOutput.WriteCouldNotOpenSource, file);
+                            catch (IOException)
+                            {
+                                return new Result(true, ConsoleErrorOutput.WriteCouldNotOpenSource, file);
+                            }
+
+                            // updates the metainformationobject for every file
+                            metaInformation.AddFileSizeUncompressed(fileInformation.FileSizeUncompressed);
+                            metaInformation.IncreaseFileAmount();
+
+                            // updates the file size compressed at the before saved position
+                            archiveWriter.WriteAddedFileSizeCompressed(binaryWriter, fileSizeCompressedPosition, fileInformation);
                         }
 
-                        metaInformation.AddFileSizeUncompressed(fileInformation.FileSizeUncompressed);
-                        metaInformation.IncreaseFileAmount();
-
-                        archiveWriter.WriteAddedFileSizeCompressed(binaryWriter, fileSizeCompressedPosition, fileInformation);
+                        // gets the next directory from the directories enumerable
+                        nextDirectory = this.GetNextDirectory(directories, ++counter);
                     }
 
+                    // writes the updated meta information to the file
                     archiveWriter.WriteChangedMetaInformation(binaryWriter, metaInformation.FilesSizeUncompressed, metaInformation.FileAmount);
                 }
 
@@ -82,6 +114,11 @@
             {
                 return new Result(true, ConsoleErrorOutput.WriteCouldNotOpenDestination, parsedArguments.Destination);
             }
+        }
+
+        private string? GetNextDirectory(IEnumerable<string> directories, int counter)
+        {
+            return directories.ElementAtOrDefault(counter);
         }
     }
 }
